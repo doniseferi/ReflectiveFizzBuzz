e4c15bb7fb39e84934c944f31a1b2c1cb57cfbb6
Edonis Seferi <edonis.seferi@liberis.com>
2024-01-24 15:12:46 +0000

    simple domain implementation to pass tests

--- src/ReflectiveFizzBuzz.Domain/Services/FizzBuzzService.cs
+++ src/ReflectiveFizzBuzz.Domain/Services/FizzBuzzService.cs
@@ -4,5 +4,12 @@ namespace ReflectiveFizzBuzz.Domain.Services;
 
 internal class FizzBuzzService : IFizzBuzzService
 {
-    public string Classify(PositiveInteger number) => number.Value.ToString();
+    public string Classify(PositiveInteger number) =>
+        number .Value switch
+        {
+            var value when value % 15 == 0 => "FizzBuzz",
+            var value when value % 3 == 0 => "Fizz",
+            var value when value % 5 == 0 => "Buzz",
+            _ => number.Value.ToString()
+        };
 }
